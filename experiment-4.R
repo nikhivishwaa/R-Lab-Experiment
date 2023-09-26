fb6380c45b63e8ce1cc98415e2a7a62aa01cb490
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-09-26 11:30:46 +0530

    Update experiment-4.R

--- experiment-4.R
+++ experiment-4.R
@@ -10,6 +10,8 @@ m<-matrix(v,2,2)
 m
 v1<-c(1,2,3,4,5,6,7,8,9)
 v1
+# exp 5 Creation of matrix
+
 m1<-matrix(v1,3,3)
 m1
 print(m)
@@ -27,4 +29,20 @@ x<-matrix(seq(2,10),3,3)
 y<-matrix(seq(12,20),3,3)
 x[1,]
 y[,1]
-x[1,]*y[,1]
+mul<-(x[1,]*y[,1])
+mul
+sum(mul)
+
+# exp 6 operations on matrix
+
+# rbind() is used for row binding & cbind for Column binding in matrix
+j<-rbind(seq(1,10))
+j
+k<-cbind(seq(1,10))
+k
+
+txfv <-rbind(seq(1,5),seq(10,14))
+txfv
+
+trxeg <-cbind(seq(4,12),seq(9,17),seq(19,27))
+trxeg
