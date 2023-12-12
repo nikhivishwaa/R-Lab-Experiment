47290001fcc3d921434307e93b3e98aa0727165b
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-12 09:56:36 +0530

    Update Exercise_1.r

--- Experiment 13/Exercise_1.r
+++ Experiment 13/Exercise_1.r
@@ -16,3 +16,13 @@ vec_stats <- function(vec){
 
 x <- c(4,5,6,2,1,4,7,8,9)
 vec_stats(x)
+
+# write a f() to print an arithmetic progression with starting value 2 and common difference 5
+
+ap <- function(a,cd){
+  for (i in 1:5){
+    print(a+(i-1)*cd)
+  }
+}
+ap(2,5)
+
